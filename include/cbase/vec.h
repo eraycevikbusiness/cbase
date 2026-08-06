9279eb28b7f674cd878e953e14aeb39cb8921a06
eraycevikbusiness <eray.cevik.business@gmail.com>
2026-08-07 00:36:15 +0200

    Implement vec allocation and growth
    Give cb_vec_t an int* buffer, add cb_vec_new, cb_vec_new_with_capacity,
    cb_vec_push, cb_vec_print and cb_vec_free, and declare cb_vec_get and
    cb_vec_set alongside a cb_vec_result_t for them to report through.
    
    Allocation and the doubling growth strategy work. Storing the pushed
    value, and the bodies of cb_vec_get and cb_vec_set, are still open.

--- include/cbase/vec.h
+++ include/cbase/vec.h
@@ -4,18 +4,28 @@
 #include <stddef.h>
 
 typedef struct {
-    void *data;
+    int *data;
     size_t capacity;
     size_t len;
 } cb_vec_t;
 
-//Instances
+typedef enum {
+    CB_VEC_RESULT_SUCCESS,
+    
+} cb_vec_result_t;
+
+
+//Make instances
 cb_vec_t cb_vec_new(void);
 cb_vec_t cb_vec_new_with_capacity(size_t cap);
 
-//"Methods"
-cb_vec_t cbvec_push(cb_vec_t *self, const int *value);
-void cb_vec_print(cb_vec_t *self);
+//Methods
+void cb_vec_push(cb_vec_t *self, const int *value);
+
+cb_vec_result_t cb_vec_get(const cb_vec_t *self, size_t index, int *res);
+cb_vec_result_t cb_vec_set(cb_vec_t *self, size_t index, int value);
+
+void cb_vec_print(const cb_vec_t *self);
 void cb_vec_free(cb_vec_t *self);
 
 #endif
--- src/vec.c
+++ src/vec.c
@@ -1,1 +1,49 @@
 #include "cbase/vec.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+#define DEFAULT_CAPACITY 16
+
+cb_vec_t cb_vec_new_with_capacity(size_t cap) {
+  cb_vec_t vec;
+  vec.capacity = cap;
+  vec.len = 0;
+  vec.data = malloc(sizeof(int) * cap);
+  return vec;
+}
+
+cb_vec_t cb_vec_new() {
+  return cb_vec_new_with_capacity(DEFAULT_CAPACITY);  
+}
+
+void cb_vec_push(cb_vec_t *self, const int *value) {
+  if (self->len >= self->capacity) {
+    //realloc
+    size_t new_cap = self->capacity * 2;
+    while (new_cap < self->capacity) new_cap *=2;
+    self->capacity = new_cap;
+    self->data = realloc(self->data, sizeof(int) * new_cap);
+  }
+
+  self->data[self->len];
+  self->len++;
+  
+}
+
+
+void cb_vec_print(const cb_vec_t *self) {
+  printf("[");
+  for (size_t i = 0; i < self->len; i++) {
+    printf("%d", self->data[i]);
+    if (i < self->len - 1) {
+      printf(", ");
+    }
+  }
+}
+
+
+void cb_vec_free(cb_vec_t *self) {
+  free(self->data);
+  self->data = NULL;
+  
+}
